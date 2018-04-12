da7f03799bee09d4722951454822869f39863e0d
Joshua Russell <jkrussell756@gmail.com>
2018-04-12 14:06:11 -0400

    set up index.js, make structure for App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    <Router>
+      <div className="App">
+        <h1>Test Div</h1>
+      </div>
+    </Router>
+  }
+}
+
+export default App;
