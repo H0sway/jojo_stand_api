3edf087985e7ec422163b2641a034b7c212f693f
Joshua Russell <jkrussell756@gmail.com>
2018-04-12 13:28:52 -0400

    set up app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+// Import dependencies
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require('path');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static('build'));
+
+// Routes
+
+app.use('/api/stands', require('/routes/stand-routes'));
+app.use('/*', (req,res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+// Set up the port
+
+app.listen(PORT, () => {
+  console.log(`Live on port ${PORT}. ORA!`);
+});
